23c395effa2e5b055dbbffcada10936d10544c4e
Abishan <abishanjeyarajah@gmail.com>
2025-08-06 11:04:40 +0530

    Add callback-func.js and update iife-func.js examples
    Added a new empty file callback-func.js. Updated iife-func.js to include comments and an example of an IIFE with parameters.

--- iife-func.js
+++ iife-func.js
@@ -1,10 +1,13 @@
 // Immediately Invoked Function Expression 
 //this is a self executing function
 //this is also called anonymous function
+
+//default
 (function() {
     console.log("iife function");
 })();
 
+//with parameter
 (function(name) {
     console.log(`My name is ${name}`);
 })("Abishan");
